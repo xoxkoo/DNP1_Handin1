7f9d3fa222c0ab312a27b16ad6fe6d3cb58a2288
AndrejSzigl <99975472+LazyTurtle518@users.noreply.github.com>
2023-01-03 17:03:22 +0100

    d

--- FileData/DAOs/UserFileDao.cs
+++ FileData/DAOs/UserFileDao.cs
@@ -13,22 +13,22 @@ public class UserFileDao : IUserDao
 		this.context = context;
 	}
 
-	public Task<User> CreateAsync(User user)
-	{
-		int userId = 1;
-		if (context.Users.Any())
+		public Task<User> CreateAsync(User user)
 		{
-			userId = context.Users.Max(u => u.Id);
-			userId++;
+			int userId = 1;
+			if (context.Users.Any())
+			{
+				userId = context.Users.Max(u => u.Id);
+				userId++;
+			}
+
+			user.Id = userId;
+
+			context.Users.Add(user);
+			context.SaveChanges();
+
+			return Task.FromResult(user);
 		}
-
-		user.Id = userId;
-
-		context.Users.Add(user);
-		context.SaveChanges();
-
-		return Task.FromResult(user);
-	}
 
 	public Task<User?> GetByUsernameAsync(string userName)
 	{
